f803a71158640c75b61302a1c74307796b393137
piomin <piotr.minkowski@gmail.com>
2021-06-02 13:32:47 +0200

    adding collections test

--- src/test/java/pl/piomin/java/samples/unmodifiablecollections/CollectionsCreatorTest.java
+++ src/test/java/pl/piomin/java/samples/unmodifiablecollections/CollectionsCreatorTest.java
@@ -1,17 +1,30 @@
 package pl.piomin.java.samples.unmodifiablecollections;
 
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class CollectionsCreatorTest {
 
     @Test
-    void shouldThrow() {
-//        CollectionsCreator creator = new CollectionsCreator();
-//        creator.fruits().add("strawberry");
-
-        Arrays.asList("apple", "banana", "orange").add("strawberry");
+    void listFormArray() {
+        List<String> list = Arrays.asList("A", "B", "C");
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> list.add("D"));
     }
 
+    @Test
+    void unmodifiableList() {
+        List<String> list = List.of("A", "B", "C");
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> list.add("D"));
+    }
+
+    @Test
+    void unmodifiableList2() {
+        ImmutableList<String> list = Lists.immutable.of("A", "B", "C");
+        Assertions.assertEquals(3, list.size());
+    }
 }
